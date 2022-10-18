ce352162797eebdaae099b8f2d65f2313d371a1a
nataliecrawford <crawfon@email.sc.edu>
2022-10-18 10:51:34 -0400

    stub dataloader datawriter

--- camp/DataLoader.java
+++ camp/DataLoader.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+/**
+ * @author Natalie Crawford
+ * The DataLoader class
+ */
+
 public class DataLoader {
+    private ArrayList<User> users;
+    private Camp camp;
     
+    public ArrayList<User> getAllUsers(){
+        return users;
+
+    }
+    public Camp getCamp(){
+        return camp;
+
+    }
 }
--- camp/DataWriter.java
+++ camp/DataWriter.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * @author Natalie Crawford
+ * The DataWriter class
+ */
 public class DataWriter {
-    
+    public static boolean saveAllUsers(ArrayList<User> users){
+        return true;
+    }
+    public static boolean saveCamp(Camp camp){
+        return true;
+    }
 }
