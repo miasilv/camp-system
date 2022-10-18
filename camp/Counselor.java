a56c47abd027b1f468837395b356888a33a775d0
Harvey Villanueva <huntingharvey2.0@gmail.com>
2022-10-18 09:42:31 -0400

    Added guardian and counselor

---
+++ camp/Counselor.java
@@ -0,0 +1,68 @@
+package camp;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Counselor {
+    private String bio;
+    private ArrayList<Cabin> cabins;
+    private ArrayList<EmergencyContact> emergencyContacts;
+    private Date birthday;
+    private ArrayList<String> allergies;
+
+    /**
+     * Constructor for the counselor class
+     * @param name Name of the counselor
+     * @param email Email of the counselor
+     * @param password Password of the counselor
+     * @param birthday Birthday of the counselor
+     */
+    public Counselor(String name, String email, String password, Date birthday) {
+
+    }
+
+    /**
+     * Method to sign the waiver for the counselor
+     */
+    public void SignWaiver() {
+
+    }
+
+    /**
+     * Method to add the bio of the counselor
+     */
+    public void addBio() {
+
+    }
+
+    /**
+     * Method to add allergies for the counselor to take of
+     * @param allergy Allergy to take care of
+     */
+    public void addAllergies(String allergy) {
+
+    }
+
+    /**
+     * Method to remove the allergy for the counselor to take care of
+     * @param allergy Allergy to remove
+     */
+    public void removeAllergy(String allergy) {
+
+    }
+
+    /**
+     * Method to edit the allergy a counselor has to take care of
+     * @param index Index of the allergy of the counselor
+     * @param allergy Allergy to replace with
+     */
+    public void editAllergy(int index, String allergy) {
+
+    }
+
+    /**
+     * Returns a string representation of the counselor
+     */
+    public String toString() {
+        
+    }
+}
---
+++ camp/User.java
@@ -0,0 +1,59 @@
+package camp;
+import java.util.UUID;
+
+public abstract class User {
+    protected UUID userID;
+    protected String name;
+    protected String email;
+    protected String phoneNumber;
+    protected String password;
+
+    /**
+     * Constructor for the user class
+     * @param name The name of the user
+     * @param email The email of the user
+     * @param password The password for the user
+     */
+    public User(String name, String email, String password) {
+
+    }
+
+    /**
+     * Method to add a phone number for a user
+     * @param phoneNumber Phone number to add
+     */
+    public void addPhone(String phoneNumber) {
+
+    }
+
+    /**
+     * Method to change the password of the user
+     * @param password Password of the user
+     */
+    public void changePassword(String password) {
+
+    }
+
+    /**
+     * Method to change the email of the user
+     * @param email The email to change the user's to
+     */
+    public void changeEmail(String email) {
+
+    }
+
+    /**
+     * Method to change the phone number of the user
+     * @param phoneNumber Phone number of the user to change to
+     */
+    public void changePhone(String phoneNumber) {
+
+    }
+
+    /**
+     * Method to delete the user
+     */
+    public void deleteUser() {
+        
+    }
+}
