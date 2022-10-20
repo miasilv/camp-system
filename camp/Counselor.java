f30ae6b71f08b30c0c0d1d664a4691df6e3bd324
Harvey Villanueva <huntingharvey2.0@gmail.com>
2022-10-20 09:05:31 -0400

    Added phone number and getters, setters

--- camp/Counselor.java
+++ camp/Counselor.java
@@ -15,9 +15,41 @@ public class Counselor extends User{
      * @param password Password of the counselor
      * @param birthday Birthday of the counselor
      */
-    public Counselor(String name, String email, String password, Date birthday) {
-        super(name, email, password);
+    public Counselor(String name, String email, String password, Date birthday, String phoneNumber) {
+        super(name, email, password, phoneNumber);
         this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     /**
--- camp/Guardian.java
+++ camp/Guardian.java
@@ -11,8 +11,40 @@ public class Guardian extends User {
      * @param email Email of the guardian
      * @param password Password of the guardian
      */
-    public Guardian(String name, String email, String password) {
-        super(name, email, password);
+    public Guardian(String name, String email, String password, String phoneNumber) {
+        super(name, email, password, phoneNumber);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     /**
