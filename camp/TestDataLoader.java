fbf266cc45f7f7aecf1ab4c4034b008720bff70b
nataliecrawford <crawfon@email.sc.edu>
2022-11-10 21:10:55 -0500

    testing dataloader

--- camp/TestDataLoader.java
+++ camp/TestDataLoader.java
@@ -159,7 +159,6 @@ class TestDataLoader {
 		/**
 		 * guardians
 		 */
-		
 
 		guardianCampers.add(camperList.get(0));
 		guardianList.add(new Guardian(UUID.randomUUID(), "Karen Smith", "karensmith@gmail.com", "password", "8034423366", guardianCampers));
