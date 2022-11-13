7e48135c088653862ce1ca1b16524e44285d2f64
Amelia Silver <miasilver4@gmail.com>
2022-11-13 15:09:41 -0500

    tested sign in on facade

--- camp/TestCampFacade.java
+++ camp/TestCampFacade.java
@@ -1,143 +1,259 @@
 package camp;
 import org.junit.platform.*;
-
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.UUID;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestCampFacade {
-    // ------------------------- CURRENT ARRAYLISTS/OBJECTS -----------------
-    private UserList userList;
-    private CampList camplist;
-    private SessionList sessionList;
-    private CabinList cabinList;
-    private CamperList camperList;
-    private CounselorList counselorList;
-    
-    
-    private Camp camp; 
-    private ArrayList<FAQ> currentFaqList;
-    private FAQ currentFaq;
-    private ArrayList<String> currentActivityList;
-    private ArrayList<Session> currentCampSessionList;
-    private Session currentSession;
-    private ArrayList<Cabin> currentSessionCabinList;
-    private Cabin currentCabin;
-    private ArrayList<Camper> currentCabinCamperList;
-    private ArrayList<Counselor> currentCabinCounselorList;
-    private HashMap<Day, Schedule> currentScheduleHash;
-    private Schedule currentSchedule;
-    
-    private User currentUser; // the person using it
-    private Director currentDirector;
-    private Counselor currentCounselor;
-    private HashMap<String, Contact> currentContactHash;
-    private HashMap<Session, Cabin> currentCabinHash;
-    private ArrayList<String> currentCounselorAllergyList;
-    private ArrayList<Session> currentCounselorSessions;
-    private ArrayList<Cabin> currentCounselorCabins;
-    private Guardian currentGuardian;
-    private ArrayList<Camper> currentGuardianCamperList;
-    private Camper currentCamper;
-    private ArrayList<Session> currentCamperSessions;
-    private ArrayList<Cabin> currentCamperCabins;
-    private ArrayList<Medication> currentMedicationList;
-    private Medication currentMedication;
-    private ArrayList<String> currentCamperAllergyList;
-    private Contact currentContact;
-    
-    //-------------------Classes (and in some cases instance variables)
-    private static final String CAMP = "camp";
-    private static final String FAQ = "faq"; //also in camp
-    private static final String SESSION = "session"; //also in camper and camp
-    private static final String CABIN = "cabin"; //also in session, counselor, and camper
-    private static final String SCHEDULE = "schedule"; //also in cabin
-    private static final String USER = "user";
-    private static final String COUNSELOR = "counselor"; //also in cabin
-    private static final String GUARDIAN = "guardian"; 
-    private static final String CAMPER = "camper"; //also in cabin and guardian
-    private static final String MEDICATION = "medication"; //also in camper
-    private static final String CONTACT = "contact"; //also in counselor and camper
+    private CampFacade facade;
 
-    
-    //------------- INSTANCE VARIABLES (NOT OBJECTS) ------------------------
-    //camp instance variables
-    private static final String NAME = "name"; //can also use for User, Camper, and Contact
-    private static final String PRICE = "price"; //can also use for Guardian
-    private static final String RATIO = "campers per counselor";
-    private static final String ACTIVITIES = "activitiy";
+    private CampList camps = CampList.getInstance();
+	private ArrayList<Camp> campList = camps.getCamps();
+	private ArrayList<FAQ> faqs = new ArrayList<FAQ>();
+	
+	//private ArrayList<Session> campSessions = new ArrayList<Session>();
+	
+	private ArrayList<String> campActivities = new ArrayList<String>();
+	private ArrayList<Cabin> campCabins = new ArrayList<Cabin>();
 
-    //FAQ instance variables
-    private static final String QUESTION = "question";
-    private static final String ANSWER = "answer";
-    
-    //session instance variables
-    private static final String THEME = "session theme";
-    private static final String SESS_DESCR = "session description"; 
-    private static final String START_DATE = "start date";
-    private static final String END_DATE = "enddate";
+	private CounselorList counselors = CounselorList.getInstance();
+	private ArrayList<Counselor> counselorList = counselors.getCounselors();
 
-    //cabin instance variables
-    private static final String VITALS = "cabin vitals information";
-    private static final String MAX_AGE = "max age";
-    private static final String MIN_AGE = "min age";
 
-    //user instance variables
-    private static final String EMAIL = "email"; //can also be used for Contact
-    private static final String PHONE = "phone number"; //can also be used for Contact
-    private static final String PASSWORD = "password";
+	private CamperList campers = CamperList.getInstance();
+	private ArrayList<Camper> camperList = campers.getCampers();
+	
 
-    //guardian instance variables
-    private static final String SESS_NUM = "total number of sessions";
+	private SessionList sessions = SessionList.getInstance();
+	private ArrayList<Session> sessionList = sessions.getSessions();
+	
+	private CabinList cabins = CabinList.getInstance();
+	private ArrayList<Cabin> cabinList = cabins.getCabins();
+	
 
-    //counselor instance variables
-    private static final String BIO = "bio";
-    private static final String BIRTHDAY = "birthday"; //can also be used for Camper
-    private static final String ALLERGIES = "allergy"; //can also be used for Camper
+	private UserList users = UserList.getInstance();
+	private ArrayList<Director> directorList = users.getDirectors();
+	private ArrayList<Guardian> guardianList = users.getGuardians();
 
-    //contact instance variables
-    private static final String RELATIONSHIP = "relationship";
-    private static final String ADDRESS = "address";
 
-    //medication instance variables
-    private static final String DOSE = "dose";
-    private static final String TYPE = "type";
-    private static final String TIME = "time";
+	@BeforeEach
+	public void setup() throws ParseException {
+		SimpleDateFormat formatter = new SimpleDateFormat("mm/dd/yyyy");  
+		camperList.clear();
+		counselorList.clear();
+		cabinList.clear();
+		sessionList.clear();
+		directorList.clear();
+		guardianList.clear();
+		campList.clear();
 
-    @BeforeEach
-    public void setup() {
-        camperList = CamperList.getInstance();
-        counselorList = CounselorList.getInstance();
-        cabinList = CabinList.getInstance();
-        sessionList = SessionList.getInstance();
-        userList = UserList.getInstance();
-        camplist = CampList.getInstance();
-        initArrayLists();
-        camp = camplist.getCamps().get(0);
-    }
 
-    private void initArrayLists() {
-        currentActivityList = new ArrayList<String>();
-        currentCampSessionList = new ArrayList<Session>();
-        currentSessionCabinList = new ArrayList<Cabin>();
-        currentCabinCamperList = new ArrayList<Camper>();
-        currentCabinCounselorList = new ArrayList<Counselor>();
-        currentGuardianCamperList = new ArrayList<Camper>();
-        currentMedicationList = new ArrayList<Medication>();
-        currentCamperAllergyList = new ArrayList<String>();
-        currentCamperSessions = new ArrayList<Session>();
-        currentCamperCabins = new ArrayList<Cabin>();
-        currentCounselorAllergyList = new ArrayList<String>();
-        currentScheduleHash = new HashMap<Day, Schedule>();
-        currentContactHash = new HashMap<String, Contact>();
-        currentCabinHash = new HashMap<Session, Cabin>();
-    }
+
+		campActivities.add("wake up" );
+        campActivities.add("breakfast at the mess hall");
+		campActivities.add("lunch at the mess hall");
+		campActivities.add("snack/rest time at cabin");
+		campActivities.add("dinner at the mess hall");
+        campActivities.add("campfire songs at firepit");
+        campActivities.add("lights out");
+        campActivities.add("ziplining at zipline track");
+        campActivities.add("swimming in lake");
+        campActivities.add("running on the field");
+        campActivities.add("gaga ball at the gaga ball pit");
+        campActivities.add("soccer on the field");
+        campActivities.add("kayaking in the lake");
+        campActivities.add("bracelet making in the mess hall");
+        campActivities.add("arts and crafts at craft building");
+        campActivities.add("painting at the craft building");
+        campActivities.add("archery at the archery range");
+        campActivities.add("tie dying at the craft building");
+        campActivities.add("singing at the mess hall");
+
+
+		/**
+		 * director
+		 */
+
+
+		directorList.add(new Director(UUID.randomUUID(), "bob wright", "bobwright88@yahoo.com", "password", "8045569988"));
+		
+		
+
+		/**
+		 * counselor
+		 */
+		
+		ArrayList<String> counselor1Allergies = new ArrayList<String>();
+		ArrayList<String> counselor1Relations = new ArrayList<String>();
+		ArrayList<Contact> counselor1Contacts = new ArrayList<Contact>();
+		ArrayList<String> counselor1Themes = new ArrayList<String>();
+		String bday = "04/22/2000";  
+		Date birthday = formatter.parse(bday); 
+		counselor1Allergies.add("blueberries");
+		counselor1Relations.add("Friend");
+		counselor1Contacts.add(new Contact("britney brock", "9903342233", "44 East Erie Street", "bb009@yahoo.com"));
+		counselor1Themes.add("hawaiian");
+		counselorList.add(new Counselor(UUID.randomUUID(), "John Messy", "johnmessy@yahoo.com", "password","899003349", "hello my name is john messy", counselor1Relations, counselor1Contacts, birthday, counselor1Allergies, counselor1Themes));
+	
+		ArrayList<String> counselor2Allergies = new ArrayList<String>();
+		ArrayList<String> counselor2Relations = new ArrayList<String>();
+		ArrayList<Contact> counselor2Contacts = new ArrayList<Contact>();
+		ArrayList<String> counselor2Themes = new ArrayList<String>();
+
+		bday = "05/28/2000";  
+		birthday = formatter.parse(bday); 
+		counselor2Allergies.add("pollen");
+		counselor2Relations.add("Friend");
+		counselor2Contacts.add(new Contact("bethany bahr", "9903345567", "12 Land Stone Street", "bethybahr@gmail.com"));
+		counselor2Themes.add("cheetah girls");
+		counselorList.add(new Counselor(UUID.randomUUID(), "Mackenzie McIntyre", "mackmack@yahoo.com", "password","1012334569", "hello my name is mackenzie mcintyre", counselor2Relations, counselor2Contacts, birthday, counselor2Allergies, counselor2Themes));
+		
+		
+
+		/**
+		 * campers
+		 */
+		
+
+		ArrayList<Medication> camper1Meds = new ArrayList<Medication>();
+		ArrayList<String> camper1Allergies = new ArrayList<String>();
+		ArrayList<String> camper1Relations = new ArrayList<String>();
+		ArrayList<Contact> camper1Contacts = new ArrayList<Contact>();
+		ArrayList<String> camper1Themes = new ArrayList<String>();
+		
+		bday = "02/13/2011";  
+		birthday = formatter.parse(bday); 
+		camper1Meds.add(new Medication("one pill", "advil", "whenever he has a headache"));
+		camper1Allergies.add("strawberries");
+		camper1Relations.add("Father");
+		camper1Contacts.add(new Contact("billy smith", "8034556688", "122 Rock Point Rd", "billysmith33@gmail.com"));
+		camper1Themes.add("hawaiian");
+		camperList.add(new Camper(UUID.randomUUID(), "bob smith", birthday,camper1Meds, camper1Allergies, camper1Relations, camper1Contacts, camper1Themes));
+		
+		ArrayList<Medication> camper2Meds = new ArrayList<Medication>();
+		ArrayList<String> camper2Allergies = new ArrayList<String>();
+		ArrayList<String> camper2Relations = new ArrayList<String>();
+		ArrayList<Contact> camper2Contacts = new ArrayList<Contact>();
+		ArrayList<String> camper2Themes = new ArrayList<String>();
+
+		bday = "06/14/2014";
+		birthday = formatter.parse(bday); 
+		camper2Meds.add(new Medication("one puff", "albuterol", "during asthma attacks"));
+		camper2Allergies.add("pollen");
+		camper2Allergies.add("peanuts");
+		camper2Relations.add("Doctor");
+		camper2Contacts.add(new Contact("Catherine Steen", "8803345578", "33 Lake Shore Drive", "drsteensoffice@gmail.com"));
+		camper2Themes.add("cheetah girls");
+		camperList.add(new Camper(UUID.randomUUID(), "josie king", birthday,camper2Meds, camper2Allergies, camper2Relations, camper2Contacts, camper2Themes));
+
+		
+
+		/**
+		 * guardians
+		 */
+		ArrayList<Camper> guardian1Campers = new ArrayList<Camper>();
+		guardian1Campers.add(camperList.get(0));
+		guardianList.add(new Guardian(UUID.randomUUID(), "Karen Smith", "karensmith@gmail.com", "password", "8034423366", guardian1Campers));
+		
+		ArrayList<Camper> guardian2Campers = new ArrayList<Camper>();
+		
+		guardian2Campers.add(camperList.get(1));
+		guardianList.add(new Guardian(UUID.randomUUID(), "Matt King", "mattking@gmail.com", "password", "9993324456", guardian2Campers));
+
+		
+		
+		/**
+		 * cabins
+		 */
+		
+		ArrayList<Camper> cabin1Campers = new ArrayList<Camper>();
+		ArrayList<Counselor> cabin1Counselors = new ArrayList<Counselor>();
+		ArrayList<Schedule> cabin1Schedules = new ArrayList<Schedule>();
+
+		for(int i = 0; i<camperList.size(); i++){
+			if(i<camperList.size()/2){
+				cabin1Campers.add(camperList.get(i));
+			}
+		}
+
+		for(int i = 0; i<7; i++){
+			cabin1Schedules.add(new Schedule(campActivities, 1));
+		}
+
+		cabin1Counselors.add(counselorList.get(0));
+		
+		cabinList.add(new Cabin(cabin1Campers, cabin1Counselors, 12.0, 10.0, cabin1Schedules, UUID.randomUUID() ));
+		
+		ArrayList<Camper> cabin2Campers = new ArrayList<Camper>();
+		ArrayList<Counselor> cabin2Counselors = new ArrayList<Counselor>();
+		ArrayList<Schedule> cabin2Schedules = new ArrayList<Schedule>();
+		
+		for(int i = 0; i<camperList.size(); i++){
+			if(i>=camperList.size()/2){
+				cabin2Campers.add(camperList.get(i));
+			}
+		}
+		for(int i = 0; i<7; i++){
+			cabin2Schedules.add(new Schedule(campActivities, 1));
+		}
+
+		cabin2Counselors.add(counselorList.get(0));
+		
+		cabinList.add(new Cabin(cabin2Campers, cabin2Counselors, 9.0, 7.0, cabin2Schedules, UUID.randomUUID() ));
+
+		
+
+		/**
+		 * sessions
+		 */
+		
+
+		ArrayList<Cabin> session1Cabins = new ArrayList<Cabin>();
+		session1Cabins.add(cabinList.get(0));
+		String start = "07/01/2023";
+		Date startDate = formatter.parse(start);      
+		String end = "07/14/2023";
+		Date endDate = formatter.parse(end);     
+		sessionList.add(new Session(UUID.randomUUID(), "cheetah girls", session1Cabins, "lets go girls", startDate, endDate));
+		
+		ArrayList<Cabin> session2Cabins = new ArrayList<Cabin>();
+		session2Cabins.add(cabinList.get(1));
+		start = "07/01/2023";   
+		startDate = formatter.parse(start);      
+		end = "07/14/2023";
+		endDate = formatter.parse(end);     
+		sessionList.add(new Session(UUID.randomUUID(), "hawaiian", session2Cabins, "aloha", startDate, endDate));
+		
+		
+		
+		/**
+		 * camp
+		 */
+		
+
+		faqs.add(new FAQ("What is the price", "$500.50"));
+
+		campList.add(new Camp(UUID.randomUUID(), "247",sessionList, 500.50, faqs,8.0,campActivities));
+		
+		DataWriter.saveCampers();
+		DataWriter.saveCounselors();
+		DataWriter.saveCabin();
+		DataWriter.saveSessions();
+		DataWriter.saveDirectors();
+		DataWriter.saveGuardians();
+		DataWriter.saveCamp();
+
+        facade = new CampFacade();
+	}
 
     @AfterEach
     public void tearDown() {
@@ -157,64 +273,69 @@ public class TestCampFacade {
         DataWriter.saveCamp();
     }
 
-    // ***************************** CAMP CLASS *********************************************************
-    @Test
-    public void testRemoveCampFAQ() {
-        boolean isRemoved;
-        isRemoved = camp.removeFAQ(0);    
-        assertFalse(isRemoved);
-    }
-
-    @Test
-    public void testAddCampFAQ() {
-        
-    }
-
-
-    // ***************************** FAQ CLASS **********************************************************
-
-
-
-    // ***************************** SESSION CLASS ******************************************************
-
-
-
-    // ***************************** CABIN CLASS ********************************************************
-
-
-
-    // ***************************** SCHEDULE CLASS *****************************************************
-
-
-
-    // ***************************** USER CLASS *********************************************************
+   //------------------------------------------------- TESTING SIGN IN'S ----------------------------
     @Test
     public void testValidCounselorSignIn() {
-        String email = userList.getCounselors().get(0).getEmail();
-        
+        String email = counselors.getCounselors().get(0).getEmail();
+        String password = counselors.getCounselor(email).getPassword();
+        boolean signedIn = facade.signIn(email, password);
+        assertTrue(signedIn);
     }
 
+    @Test
+    public void testInvalidPasswordCounselorSignIn() {
+        String email = counselors.getCounselors().get(0).getEmail();
+        boolean signedIn = facade.signIn(email, "notapassword");
+        assertFalse(signedIn);
+    }
 
-    // ***************************** DIRECTOR CLASS *****************************************************
+    @Test
+    public void testInvalidEmailCounselorSignIn() {
+        boolean signedIn = facade.signIn("notanemail", "notapassword");
+        assertFalse(signedIn);
+    }
 
+    @Test
+    public void testValidGuardianSignIn() {
+        String email = users.getGuardians().get(0).getEmail();
+        String password = users.getGuardian(email).getPassword();
+        boolean signedIn = facade.signIn(email, password);
+        assertTrue(signedIn);
+    }
 
+    @Test
+    public void testInvalidPasswordGuardianSignIn() {
+        String email = users.getGuardians().get(0).getEmail();
+        boolean signedIn = facade.signIn(email, "notapassword");
+        assertFalse(signedIn);
+    }
 
-    // ***************************** COUNSELOR CLASS ****************************************************
+    @Test
+    public void testInvalidEmailGuardianSignIn() {
+        boolean signedIn = facade.signIn("notanemail", "notapassword");
+        assertFalse(signedIn);
+    }
 
+    @Test
+    public void testValidDirectorSignIn() {
+        String email = users.getDirectors().get(0).getEmail();
+        String password = users.getDirector(email).getPassword();
+        boolean signedIn = facade.signIn(email, password);
+        assertTrue(signedIn);
+    }
 
+    @Test
+    public void testInvalidPasswordDirectorSignIn() {
+        String email = users.getDirectors().get(0).getEmail();
+        boolean signedIn = facade.signIn(email, "notapassword");
+        assertFalse(signedIn);
+    }
 
-    // ***************************** GUARDIAN CLASS *****************************************************
+    @Test
+    public void testInvalidEmailDirectorSignIn() {
+        boolean signedIn = facade.signIn("notanemail", "notapassword");
+        assertFalse(signedIn);
+    }
 
-
-
-    // ***************************** CAMPER CLASS *******************************************************
-
-
-
-    // ***************************** MEDICATION CLASS ***************************************************
-    
-    
-    
-    // ***************************** CONTACTS CLASS *****************************************************
 
 }
